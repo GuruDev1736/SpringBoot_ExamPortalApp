16f72f5d858069f9a012c75d3364b9ba9c4984a6
gurudev <guruprasad1736@gmail.com>
2024-06-04 23:12:01 +0530

    user is remmaining

---
+++ src/main/java/com/GuruDev/ExamPortal/Model/UserRole.java
@@ -0,0 +1,32 @@
+package com.GuruDev.ExamPortal.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userRoleId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    @ManyToOne
+    private Role role;
+
+}
